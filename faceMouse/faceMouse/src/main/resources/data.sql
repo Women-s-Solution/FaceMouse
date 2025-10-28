ba45e769a6492f6a007b974f3643a9439d80d69b
EveAchnitz <eve.jo.achnitz@gmail.com>
2025-10-28 07:42:21 -0300

    Criação do data.sql
    Criação do data.sql com um usuário já cadastro.

---
+++ faceMouse/faceMouse/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+/*Inserindo o primeiro id da tabela*/
+insert ignore into tb_usuario (id_usuario, nome, email, telefone, assunto, mensagem) values
+(1,'Carmen', 'carmen.alice@gmail.com', '11232345454','Interesse no sistema','Olá, sou diretora de uma escola em Sorocaba e gostaria de aplicar o Face Mouse nos nossos notebooks.');
