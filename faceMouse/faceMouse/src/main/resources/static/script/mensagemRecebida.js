1adfb93063d3e9050e99e1e60f25e0054bb5a800
EveAchnitz <eve.jo.achnitz@gmail.com>
2025-11-04 13:04:34 -0300

    Página de mensagens recebidas
    Página de mensagens recebidas com o logout do administrador.

--- faceMouse/faceMouse/src/main/resources/static/script/mensagemRecebida.js
+++ faceMouse/faceMouse/src/main/resources/static/script/mensagemRecebida.js
@@ -1,34 +1,39 @@
 // Espera o carregamento completo do DOM antes de executar o código
 document.addEventListener("DOMContentLoaded", () => {
+   fetch("http://localhost:8080/usuarios")// consulta os dados dos usuários
+   .then(response => response.json())// converte para json
+   .then(usuarios => {
+	const container = document.getElementById("lista-mensagens");// armazena o espaço da section do html na const container
+	
+	if(usuarios.length === 0){// se não tiver dados nos usuários
+		container.innerHTML = "<p>Nenhuma mensagem foi enviada.</p>";
+		return;
+	}
+	
+	usuarios.forEach(usuario => {// para cada usuário
+		const card = document.createElement("div");// cria uma div
+		card.classList.add("mensagem-card");// com uma classe chamada 'mensagem-card'
+		
+		// adiciona no html um card com essas informações
+		card.innerHTML = `
+		<h3>${usuario.nome}</h3>
+		<h5>${usuario.email}</h5>
+		<h4>${usuario.assunto}</h4>
+		<p>${usuario.mensagem}</p>`;
+		
+		container.appendChild(card);// appendChild insere o card (elemento filho) dentro do container (elemento pai)
+	});
+   })
+   .catch(error => { // caso dê erro
+	console.error("Erro ao carregar mensagens :(", error);
+	document.getElementById("lista-mensagens").innerHTML =
+	"<p>Erro ao carregar mensagens. Tente novamente depois.</p>";
+   });
    
-    // Obtém os parâmetros da URL
-    const urlParams = new URLSearchParams(window.location.search);
-   
-    // Pega o valor do parâmetro "id"
-    const usuarioId = urlParams.get("id");
-   
-    // Se houver um ID do usuario na URL
-    if (usuarioId) {
-       
-        // Faz uma requisição ao backend para buscar os dados do usuario (inclusive a mensagem)
-        fetch(`http://localhost:8080/usuarios/${usuarioId}`)
-            .then(response => response.json()) // Converte a resposta para JSON
-            .then(usuario => {
-				
-                // Atualiza o autor da mensagem
-                document.getElementById("usuario-nome").textContent = usuario.nome;
-				
-				//Atualiza o email do usuario
-				document.getElementById("usuario-email").textContent = usuario.email;
-				
-				//Atualiza o assunto da mensagem
-                document.getElementById("usuario-assunto").textContent = usuario.assunto;
-
-                // Atualiza o texto da mensagem
-                document.getElementById("usuario-mensagem").textContent = usuario.mensagem;
-               
-            })
-            // Exibe erro no console se a requisição falhar
-            .catch(error => console.error("Erro ao carregar as mensagens:", error));
-    }
+   //Logout   
+   document.getElementById('btn-logout').addEventListener('click', () => {
+       localStorage.removeItem('adminLogado');
+       alert('Logout efetuado!');
+       window.location.href = 'index.html';
+    });
 });
